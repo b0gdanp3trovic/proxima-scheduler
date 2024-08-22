f7bad846df2f7a3c2cf3f215f736fa744e9a9789
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2024-08-23 00:25:19 +0200

    Pinger config

---
+++ proxima-scheduler/pinger/config.go
@@ -0,0 +1,63 @@
+package pinger
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+type PingerConfig struct {
+	InfluxDBAddress string
+	DatabaseName    string
+	DatabaseEnabled bool
+	PingInterval    time.Duration
+}
+
+func LoadConfig() *PingerConfig {
+	influxDBAddress := getEnv("INFLUXDB_ADDRESS", "http://localhost:8086")
+	databaseName := getEnv("INFLUXDB_DB_NAME", "ping_db")
+	databaseEnabled := getEnvAsBool("DATABASE_ENABLED", true)
+	pingInterval := getEnvAsDuration("PING_INTERVAL", 10*time.Second)
+
+	return &PingerConfig{
+		InfluxDBAddress: influxDBAddress,
+		DatabaseName:    databaseName,
+		DatabaseEnabled: databaseEnabled,
+		PingInterval:    pingInterval,
+	}
+}
+
+func getEnv(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func getEnvAsBool(name string, defaultVal bool) bool {
+	valStr := getEnv(name, "")
+	if valStr == "" {
+		return defaultVal
+	}
+	val, err := strconv.ParseBool(valStr)
+	if err != nil {
+		log.Printf("Invalid boolean value %s for %s, using default %t", valStr, name, defaultVal)
+		return defaultVal
+	}
+	return val
+}
+
+func getEnvAsDuration(name string, defaultVal time.Duration) time.Duration {
+	valStr := getEnv(name, "")
+	if valStr == "" {
+		return defaultVal
+	}
+	val, err := time.ParseDuration(valStr)
+	if err != nil {
+		log.Printf("Invalid duration value %s for %s, using default %s", valStr, name, defaultVal)
+		return defaultVal
+	}
+	return val
+}
