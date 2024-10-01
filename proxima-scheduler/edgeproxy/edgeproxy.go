bf1373f17beb6d5e8e92895e9f448c1e6793ae32
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2024-10-01 23:26:33 +0200

    Fetch pods from consul wip

--- proxima-scheduler/edgeproxy/edgeproxy.go
+++ proxima-scheduler/edgeproxy/edgeproxy.go
@@ -2,6 +2,9 @@ package edgeproxy
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -9,11 +12,19 @@ import (
 	"time"
 )
 
-type EdgeProxy struct {
-	proxy *httputil.ReverseProxy
+type ConsulServiceInstance struct {
+	Service struct {
+		Address string `json:"Address"`
+		Port    int    `json:"Port"`
+	} `json:"Service"`
 }
 
-func NewEdgeProxy() *EdgeProxy {
+type EdgeProxy struct {
+	proxy         *httputil.ReverseProxy
+	consulAddress string
+}
+
+func NewEdgeProxy(consulAddress string) *EdgeProxy {
 	return &EdgeProxy{
 		proxy: &httputil.ReverseProxy{
 			Director: func(req *http.Request) {
@@ -28,10 +39,19 @@ func NewEdgeProxy() *EdgeProxy {
 					return
 				}
 
-				serviceName := parts[1] + ".default.svc.cluster.local"
+				serviceName := parts[1]
+				pods, err := getServicePodsFromConsul(serviceName, consulAddress)
+				if err != nil || len(pods) == 0 {
+					log.Printf("Failed to get service instances from Consul: %v", err)
+					req.URL.Host = ""
+					return
+				}
+
+				pod := pods[0]
+
 				req.URL.Scheme = "http"
-				req.URL.Host = serviceName
-				log.Printf("Forwarding request to %s", serviceName)
+				req.URL.Host = fmt.Sprintf("%s:%d", pod.Service.Address, pod.Service.Port)
+				log.Printf("Forwarding request to %s:%d", pod.Service.Address, pod.Service.Port)
 			},
 			ModifyResponse: func(resp *http.Response) error {
 				// Measure latency and log it
@@ -42,6 +62,27 @@ func NewEdgeProxy() *EdgeProxy {
 			},
 		},
 	}
+}
+
+func getServicePodsFromConsul(serviceName string, consulURL string) ([]ConsulServiceInstance, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/v1/health/service/%s?passing=true", consulURL, serviceName))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pods []ConsulServiceInstance
+	err = json.Unmarshal(body, &pods)
+	if err != nil {
+		return nil, err
+	}
+
+	return pods, nil
 }
 
 func (ep *EdgeProxy) Run() {
