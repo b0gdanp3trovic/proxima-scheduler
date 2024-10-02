9625c2129e6fc1d0be661231c71882423725a721
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2024-10-02 22:50:18 +0200

    Use a handler in edgeproxy

--- proxima-scheduler/edgeproxy/edgeproxy.go
+++ proxima-scheduler/edgeproxy/edgeproxy.go
@@ -28,19 +28,8 @@ func NewEdgeProxy(consulAddress string) *EdgeProxy {
 	return &EdgeProxy{
 		proxy: &httputil.ReverseProxy{
 			Director: func(req *http.Request) {
-				log.Println("Got a request")
-				// Set start time in the request context
-				ctx := context.WithValue(req.Context(), "start_time", time.Now())
-				req = req.WithContext(ctx)
+				serviceName := req.Context().Value("service_name").(string)
 
-				parts := strings.Split(req.URL.Path, "/")
-				if len(parts) < 2 || parts[1] == "" {
-					log.Println("Invalid path, cannot extract service name")
-					req.URL.Host = "" // Force a failure by clearing the Host
-					return
-				}
-
-				serviceName := parts[1]
 				pods, err := getServicePodsFromConsul(serviceName, consulAddress)
 				if err != nil || len(pods) == 0 {
 					log.Printf("Failed to get service instances from Consul: %v", err)
@@ -50,10 +39,12 @@ func NewEdgeProxy(consulAddress string) *EdgeProxy {
 
 				pod := pods[0]
 
+				// Forward the request to the pod
 				req.URL.Scheme = "http"
 				req.URL.Host = fmt.Sprintf("%s:%d", pod.Service.Address, pod.Service.Port)
 				log.Printf("Forwarding request to %s:%d", pod.Service.Address, pod.Service.Port)
 			},
+
 			ModifyResponse: func(resp *http.Response) error {
 				// Measure latency and log it
 				latency := time.Since(resp.Request.Context().Value("start_time").(time.Time))
@@ -86,13 +77,30 @@ func getServicePodsFromConsul(serviceName string, consulURL string) ([]ConsulSer
 	return pods, nil
 }
 
+func preprocessRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) < 2 || parts[1] == "" {
+			http.Error(w, "Invalid request path", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "service_name", parts[1])
+
+		ctx = context.WithValue(ctx, "start_time", time.Now())
+
+		// Pass the request to the next handler (the proxy)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func (ep *EdgeProxy) Run() {
 	// Start the proxy server in a goroutine
 	go func() {
 		log.Println("Edge proxy server is starting on port 8080...")
 
 		// Set up the HTTP handler to use the proxy
-		http.Handle("/", ep.proxy)
+		http.Handle("/", preprocessRequest(ep.proxy))
 
 		// Start listening and serving on port 8080
 		err := http.ListenAndServe(":8080", nil)
