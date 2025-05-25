1103c4d39592c3b8e99f14f9830d198f5469f08c
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2025-05-25 13:47:04 +0200

    Testservice improve for debug

--- testservice/app.py
+++ testservice/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hi! This is a test service."
+    pod_name = os.environ.get("POD_NAME", "unknown-pod")
+    return f"Hi! This is a test service running in pod: {pod_name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
